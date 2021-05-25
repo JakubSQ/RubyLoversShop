bfd683584538514b16faf7e11ce2c508dc716417
JakubSQ <skuta.jakub@gmail.com>
2021-05-25 19:14:22 +0200

    Fixed product controller

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  subject { build(:product)}
+  let(:product) { create(:product) }
 
   it "name should be present" do
-    subject.name = nil
-    expect(subject).to_not be_valid
+    product.name = nil
+    expect(product).to_not be_valid
   end
 
   it "description should be present" do
-    subject.description = nil
-    expect(subject).to_not be_valid
+    product.description = nil
+    expect(product).to_not be_valid
   end
 
   it "photo should be present" do
-    subject.cover_photo = nil
-    expect(subject).to_not be_valid
+    product.cover_photo = nil
+    expect(product).to_not be_valid
   end
-
 end
