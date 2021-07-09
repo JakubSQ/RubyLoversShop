2975d19ee07b0d68147e9d909d249824ada4d919
JakubSQ <41249878+JakubSQ@users.noreply.github.com>
2021-07-09 09:37:44 +0200

    Seeds added (#13)
    * Seeds added
    
    * Fixed duplication in porducts_controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create(email: 'user@example', password: 'password')
+Admin.create(email: 'admin@example', password: 'password')
+
+3.times do
+  category = Category.create(title: Faker::Commerce.department(max: 1, fixed_amount: true))
+  brand = Brand.create(title: Faker::Appliance.brand)
+end
+
+10.times do
+  product = Product.create(name: Faker::Commerce.unique.product_name,
+                           prize: Faker::Number.non_zero_digit,
+                           description: Faker::Lorem.sentence,
+                           brand: Brand.all.sample,
+                           category: Category.all.sample)
+end
