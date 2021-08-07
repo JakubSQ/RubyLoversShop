001fc2d905e5e5ca96e9b8fd6e1677c471f50d5d
JakubSQ <skuta.jakub@gmail.com>
2021-08-07 10:44:47 +0200

    System spec for order list added

---
+++ spec/system/order_list_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'AdminUpdatingProducts', type: :system do
+  let!(:admin) { create(:admin) }
+  let!(:order) { Order.create }
+
+  before do
+    driven_by(:rack_test)
+    visit new_admin_session_path
+    fill_in 'admin_email', with: admin.email
+    fill_in 'admin_password', with: admin.password
+    click_on 'Log in'
+    visit admin_root_path
+  end
+
+  it 'admin sees Orders link on navigation bar' do
+    expect(page).to have_content('Orders')
+  end
+
+  it 'admin sees order list in Order page' do
+    click_on 'Orders'
+    expect(page).to have_content(order.id)
+  end
+
+  it 'admin sees pagination in Orders page' do
+    click_on 'Orders'
+    expect(page).to have_content('Next')
+    expect(page).to have_content('Prev')
+  end
+end
