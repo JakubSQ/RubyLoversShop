26b4b0984c6998f90eb38e4833ec9868699b79af
JakubSQ <skuta.jakub@gmail.com>
2021-10-29 15:13:33 +0200

    updated cart service and cart spec

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,7 +5,7 @@ class LineItemsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    add_product = CartServices::AddProduct.new.call(cart, product, params[:quantity].to_i)
+    add_product = CartServices::AddProduct.new(cart, product, params[:quantity].to_i).call
     if add_product.success?
       redirect_to cart, notice: 'Item added to cart'
     else
--- app/services/cart_services.rb
+++ app/services/cart_services.rb
@@ -2,33 +2,48 @@
 
 module CartServices
   class AddProduct
-    def call(cart, product, quantity)
-      if product_not_valid?(cart, product, quantity)
-        OpenStruct.new({ success?: false, payload: @error })
+    def initialize(cart, product, quantity)
+      @cart = cart
+      @product = product
+      @quantity = quantity
+    end
+
+    def call
+      if product_valid? || quantity_valid?
+        OpenStruct.new({ success?: true, payload: save_line_item })
       else
-        save_line_item(cart, product, quantity)
-        current_item = cart.line_items.find_by(product_id: product.id)
-        OpenStruct.new({ success?: true, payload: current_item })
+        OpenStruct.new({ success?: false, payload: current_item_errors })
       end
     end
 
     private
 
-    def product_not_valid?(cart, product, quantity)
-      current_item = cart.line_items.build(product_id: product.id, quantity: quantity)
-      current_item.valid?
-      @error = current_item.errors.full_messages
-      @error.size.positive?
+    def exist_item
+      @exist_item = @cart.line_items.find_by(product_id: @product.id)
     end
 
-    def save_line_item(cart, product, quantity)
-      current_item = cart.line_items.find_by(product_id: product.id)
-      if current_item
-        current_item.increment(:quantity, quantity.to_i)
+    def current_item
+      @current_item ||= @cart.line_items.where(product_id: @product.id, quantity: @quantity).first_or_initialize
+    end
+
+    def product_valid?
+      current_item.valid?
+    end
+
+    def quantity_valid?
+      @quantity.positive?
+    end
+
+    def current_item_errors
+      current_item.errors.full_messages || exist_item.errors.full_messages
+    end
+
+    def save_line_item
+      if exist_item
+        exist_item.increment(:quantity, @quantity).save
       else
-        current_item = cart.line_items.build(product_id: product.id, quantity: quantity)
+        current_item.save
       end
-      current_item.save
     end
   end
 end
