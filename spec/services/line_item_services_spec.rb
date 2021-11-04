5ce6d84eb0aa5e94b8c1d7304e0ee416606327e3
JakubSQ <skuta.jakub@gmail.com>
2021-11-04 15:31:07 +0100

    Updated LinItemServices spec na new system spec for product removal from spec regardless of items of this product

--- spec/services/line_item_services_spec.rb
+++ spec/services/line_item_services_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe LineItemServices, type: :model do
   let!(:product) { create(:product) }
   let!(:product1) { create(:product, name: 'trousers') }
   let!(:cart) { Cart.create }
-  let!(:line_item) { LineItem.create(cart_id: cart.id, product_id: product.id) }
-  let!(:line_item1) { LineItem.create(cart_id: cart.id, product_id: product1.id) }
+  let!(:line_item) { LineItem.create(cart_id: cart.id, product_id: product.id, quantity: 10) }
+  let!(:line_item1) { LineItem.create(cart_id: cart.id, product_id: product1.id, quantity: 10) }
 
   context 'shopping cart includes two products' do
-    it 'one product remove from cart' do
+    it 'one product having 10 instances remove from cart' do
       subject = LineItemServices::DeleteLineItem.new.call(cart, line_item)
 
       expect(cart.line_items.count).to eq(1)
       expect(subject.payload).to eq('Line Item successfully removed from shopping cart')
     end
 
-    it 'two products remove from cart' do
+    it 'two products having 10 instances remove from cart' do
       LineItemServices::DeleteLineItem.new.call(cart, line_item)
       subject = LineItemServices::DeleteLineItem.new.call(cart, line_item1)
 
---
+++ spec/system/line_item_removal_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'LineItemQuantity', type: :system do
+  let(:cart) { create(:cart) }
+  let(:product) { create(:product) }
+
+  describe 'when logged in as admin' do
+    let(:admin) { create(:admin) }
+
+    before do
+      driven_by(:rack_test)
+      visit new_admin_session_path
+      fill_in 'admin_email', with: admin.email
+      fill_in 'admin_password', with: admin.password
+      click_on 'Log in'
+      visit product_path(product)
+      fill_in 'quantity', with: 10
+      click_on 'Add to cart'
+    end
+
+    context 'is allowed to' do
+      it 'remove product from cart regardless of items of this product' do
+        click_on 'Remove'
+        expect(page).to have_content('Your shopping cart is empty')
+        expect(LineItem.find_by(product_id: product.id)).to eq(nil)
+      end
+    end
+  end
+
+  describe 'when logged in as user' do
+    let(:user) { create(:user) }
+
+    before do
+      driven_by(:rack_test)
+      visit new_user_session_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_on 'Log in'
+      visit product_path(product)
+      fill_in 'quantity', with: 10
+      click_on 'Add to cart'
+    end
+
+    context 'is allowed to' do
+      it 'remove product from cart regardless of items of this product' do
+        click_on 'Remove'
+        expect(page).to have_content('Your shopping cart is empty')
+        expect(LineItem.find_by(product_id: product.id)).to eq(nil)
+      end
+    end
+  end
+
+  describe 'when guest visits app' do
+    before do
+      driven_by(:rack_test)
+      visit product_path(product)
+    end
+
+    context 'is not allowed to' do
+      it 'visit cart path' do
+        fill_in 'quantity', with: 2
+        click_on 'Add to cart'
+        expect(page).to have_content('You are not authorized')
+      end
+    end
+  end
+end
